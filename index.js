0d79df056648258361205e32286a3fc32502753c
Gabe <gabenedden@gmail.com>
2022-07-28 16:43:52 -0400

    initialize server

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// import the needed node_modules.
+const express = require("express");
+const morgan = require("morgan");
+let port = process.env.PORT || 8000;
+
+express()
+  // Below are methods that are included in express(). We chain them for convenience.
+  // --------------------------------------------------------------------------------
+
+  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+  .use(morgan("tiny"))
+  .use(express.json())
+
+  // Any requests for static files will go into the public folder
+  .use(express.static("public"))
+
+  .get("/", (req, res) => {
+    res.send("You have arrived");
+  })
+
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
+
+  // Node spins up our server and sets it to listen on port 8000.
+  .listen(port, () => console.log(`Listening on port ${port}`));
