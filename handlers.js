c70a6d1e85552e38161445f93e3fe528c22970b6
Gabe <gabenedden@gmail.com>
2022-08-11 09:19:33 -0400

    readd handlers

--- handlers.js
+++ handlers.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require("mongodb");
-
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
-const { MONGO_URI } = process.env;
 
+const { MONGO_URI } = process.env;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,21 +10,62 @@ const options = {
 
 const test = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
-
   try {
     await client.connect();
     const db = client.db("catchup");
     console.log("connected");
 
-    return res.status(200).json({ status: 200, message: "You have arrived" });
+    res.status(200).json({ status: 200, message: "You have arrived" });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ status: 500, error: err });
   } finally {
-    await client.close();
+    client.close();
+    console.log("disconnected");
+  }
+};
+
+const getPosts = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("catchup");
+    console.log("connected");
+
+    const response = await db.collection("posts").find().toArray();
+
+    res.status(200).json({ status: 200, data: response });
+  } catch (err) {
+    res.status(500).json({ status: 500, error: err });
+  } finally {
+    client.close();
+    console.log("disconnected");
+  }
+};
+
+const getAuthUser = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  try {
+    const { authid } = req.params;
+
+    await client.connect();
+    const db = client.db("catchup");
+    console.log("connected");
+
+    const query = { _id: ObjectId(authid) };
+
+    const response = await db.collection("users").findOne(query);
+
+    res.status(200).json({ status: 200, data: response });
+  } catch (err) {
+    res.status(500).json({ status: 500, error: err });
+  } finally {
+    client.close();
     console.log("disconnected");
   }
 };
 
 module.exports = {
   test,
+  getAuthUser,
+  getPosts,
 };
